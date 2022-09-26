4c585e42dc11d5679393c9e54531ae59ef3a4eb8
Shortisc <shortisc@gmail.com>
2022-09-26 22:33:57 +0300

    Add project files.

---
+++ FirstProjectNew/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstProjectNew
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
